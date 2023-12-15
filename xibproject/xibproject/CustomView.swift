f6d64cab82c56951d83c2256f97539a675a13980
Yasin <72190310+dalkilicyasin@users.noreply.github.com>
2023-12-16 00:21:47 +0300

    Custom View

---
+++ xibproject/xibproject/CustomView.swift
@@ -0,0 +1,16 @@
+//
+//  CustomView.swift
+//  xibproject
+//
+//  Created by yasin on 16.12.2023.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
---
+++ xibproject/xibproject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  xibproject
+//
+//  Created by yasin on 16.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var customView: CustomView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
